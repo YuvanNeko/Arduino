32291944527ae9543bc380b3793d6c51649d63bf
YuvanNeko <54617539+YuvanNeko@users.noreply.github.com>
2021-06-07 10:58:34 +0530

    Add files via upload

---
+++ Alarm.ino
@@ -0,0 +1,53 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ESP8266WebServer.h>
+
+const char *ssid     = "Home Network Extended";
+const char *password = "Idonthinkyoushouldbedoingthis";
+const char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const int dt = 1000;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, 19800);
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(D4, OUTPUT);
+  digitalWrite(D4, HIGH);
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );  
+  }
+
+  timeClient.begin();
+}
+
+void alarm(){
+  digitalWrite(D4, LOW);
+  delay(dt);
+  digitalWrite(D4, HIGH);
+  delay(dt);
+}
+void printime(){
+  timeClient.update();
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  Serial.println(WiFi.localIP());
+  delay(dt);
+}
+void loop() {
+  int hr = timeClient.getHours();
+  if(hr >= 6 && hr <= 8){
+   alarm();
+  }
+    else{
+    printime();
+   }
+}
---
+++ Time.ino
@@ -0,0 +1,34 @@
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+const char *ssid     = "Home Network Extended";
+const char *password = "Idonthinkyoushouldbedoingthis";
+const char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, 19800);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  delay(1000);
+}
